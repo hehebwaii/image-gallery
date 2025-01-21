79219c34de9dde568a163fca28620307b57adf81
hehebwaii <142102178+hehebwaii@users.noreply.github.com>
2025-01-21 22:11:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 btn.addEventListener('click', () => {
-  const currentClass = btn.getAttribute('class'); // Get the current class of the button
+  const currentClass = btn.getAttribute('class');
 
   if (currentClass === 'dark') {
-    // Switch to "light" mode
     btn.setAttribute('class', 'light');
     btn.textContent = 'Lighten';
-    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'; // Apply a dark overlay
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
   } else {
-    // Switch back to "dark" mode
     btn.setAttribute('class', 'dark');
     btn.textContent = 'Darken';
-    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)'; // Remove the overlay
+    overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
   }
 });
