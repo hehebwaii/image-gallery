ab716c640341b04e99feae5a74f56b1e4780549e
hehebwaii <142102178+hehebwaii@users.noreply.github.com>
2025-01-21 22:14:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
+// References to HTML elements
+const thumbBar = document.querySelector('.thumb-bar');
+const displayedImg = document.querySelector('.displayed-img');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('button');
+
+// Array of image filenames
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+// Object containing alternative text for each image
+const imageAltTexts = {
+  'pic1.jpg': 'Closeup of a blue human eye',
+  'pic2.jpg': 'Rock formation in a desert',
+  'pic3.jpg': 'Purple and white flowers',
+  'pic4.jpg': 'Ancient temple at sunset',
+  'pic5.jpg': 'A view of the sea through cliffs',
+};
+
+// Loop through the images to create thumbnails
+for (let i = 0; i < imageFilenames.length; i++) {
+  const newImage = document.createElement('img');
+  const srcPath = `images/${imageFilenames[i]}`;
+  const altText = imageAltTexts[imageFilenames[i]];
+
+  newImage.setAttribute('src', srcPath);
+  newImage.setAttribute('alt', altText);
+  thumbBar.appendChild(newImage);
+
+  // Add click event listener to display the clicked image
+  newImage.addEventListener('click', () => {
+    displayedImg.setAttribute('src', srcPath);
+    displayedImg.setAttribute('alt', altText);
+  });
+}
+
+// Add click event listener to the "Darken/Lighten" button
 btn.addEventListener('click', () => {
   const currentClass = btn.getAttribute('class');
 
